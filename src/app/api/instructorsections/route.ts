bebc58a9e69062f0991db442f93b666922682478
Hung <hung171197@gmail.com>
2024-04-23 16:06:55 -0500

    Feat Given an instructor List all the sections that he/she has taught (given a range of semesters)

---
+++ src/app/api/instructorsections/route.ts
@@ -0,0 +1,80 @@
+import prisma from "@/lib/db";
+import { Prisma } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const semesterDates: Record<string, { start: string; end: string }> = {
+    Spring: { start: "-01-15", end: "-05-01" },
+    Summer: { start: "-06-01", end: "-08-01" },
+    Fall: { start: "-08-15", end: "-12-15" }
+} as const;
+
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = req.nextUrl;
+    const instructorId = searchParams.get('instructorId') ?? "";
+    const startSemester = searchParams.get('startSemester') ?? "";
+    const startYear = searchParams.get('startYear') ?? "";
+    const endSemester = searchParams.get('endSemester') ?? "";
+    const endYear = searchParams.get('endYear') ?? "";
+    
+    const startDate = new Date(startYear + semesterDates[startSemester].start);
+    const endDate = new Date(endYear + semesterDates[endSemester].end);
+
+    if (!instructorId || !startSemester || !endSemester) {
+        return NextResponse.json({ reason: "Missing required parameters" }, { status: 400 });
+    }
+
+    try {
+        const instructorsections = await prisma.section.findMany({
+            where: {
+                AND: [
+                    { instructorId: instructorId },
+                    { startDate: { gte: startDate } },
+                    { endDate: { lte: endDate } }
+                ],
+                
+            },
+            
+            include: {
+                course: {
+                    select: {
+                        name: true
+                        // isCore: true
+                    }
+                },
+                instructor: {
+                    select: {
+                        name: true
+                    }
+                }
+            },
+            orderBy: {
+                startDate: 'asc'
+            }
+        });
+        
+        // Transform the structure of sections to flatten them
+        const sectionsTaught = instructorsections.map(section => ({
+            sectionNumber: section.sectionNumber,
+            courseNumber: section.courseNumber,
+            courseName: section.course.name,
+            instructorId: section.instructorId,
+            instructorName: section.instructor.name,
+            semester: section.semester,
+            year: section.year,
+            num_students: section.num_students,
+            startDate: section.startDate.toISOString().slice(0, 10),
+            endDate: section.endDate.toISOString().slice(0, 10)
+        }));
+
+        if (sectionsTaught.length === 0) {
+            return NextResponse.json({ reason: "No sections found" }, { status: 404 });
+        }
+    
+        return NextResponse.json({ sectionsTaught }, { status: 200 });
+    }
+    catch (error) {
+        console.error("Fail to retrieve sections: ",error);
+        return NextResponse.json({ reason: "Internal server error" }, { status: 500 });
+    }
+}
