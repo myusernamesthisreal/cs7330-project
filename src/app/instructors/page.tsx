44167970dafd97d8dd89659eabff3cbdb715f638
Hung <hung171197@gmail.com>
2024-04-23 20:09:03 -0500

    Add front end to view the Section taught

---
+++ src/app/instructors/page.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { Instructor } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react"
+
+export default function ViewInstructors() {
+    const [instructors, setInstructors] = useState([] as Instructor[]);
+    const router = useRouter();
+    useEffect(() => {
+        const handleFetch = async () => {
+            const res = await fetch("/api/instructors");
+            const data = await res.json();
+            setInstructors(data);
+        }
+        handleFetch();
+    }, [])
+    return (
+        <>
+            <main className="flex flex-col items-center justify-between p-24">
+                <div className="z-10 max-w-3xl w-full items-center justify-between text-sm">
+                    <h1 className="text-4xl font-bold text-center mb-4">Instructors</h1>
+                    <div className="grid grid-cols-3 gap-4">
+                    {instructors.map((instructor, index) => (
+                        <div key={index} className="w-full p-4 border border-gray-300 rounded-lg mb-4">
+                            <p className="text-lg font-bold">{instructor.name}</p>
+                            <p className="text-sm">{instructor.id_number}</p>
+                            <button onClick={() => router.push(`/instructors/view?id_number=${instructor.id_number}`)} className="bg-blue-500 w-full text-white rounded-lg p-2 mt-4">View Taught</button>
+                        </div>
+                    ))}
+                    </div>
+                    <button onClick={() => router.push("/")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 self-start">Back</button>
+                </div>
+
+            </main>
+        </>
+    )
+}
---
+++ src/app/instructors/view/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+import { Section } from "@prisma/client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { FormEvent, useState } from "react"
+
+export default function ViewTaught() {
+    const [sections, setSections] = useState([] as Section[]);
+    const [startSemester, setStartSemester] = useState("");
+    const [startYear, setStartYear] = useState("");
+    const [endSemester, setEndSemester] = useState("");
+    const [endYear, setEndYear] = useState("");
+    const [error, setError] = useState(null);
+    const router = useRouter();
+    const query = useSearchParams();
+    const id_number = query.get("id_number");
+
+    const handleSubmit = async (e: FormEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setError(null);
+        setSections([]);
+
+        if (!id_number) {
+            return;
+        }
+
+        const res = await fetch(`/api/instructorsections?instructorId=${id_number}&startSemester=${startSemester}&startYear=${startYear}&endSemester=${endSemester}&endYear=${endYear}`);
+        if (res.ok) {
+            const data = await res.json();
+            setSections(data.sectionsTaught);
+        } else {
+            const data = await res.json();
+            setError(data.reason || "Something went wrong");
+        }
+    }
+
+    return (
+        <>
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <div className="z-10 max-w-xl w-full items-center justify-between text-sm lg:flex lg:flex-col">
+                    <h1 className="text-4xl font-bold text-center mb-4">Sections Taught</h1>
+                    
+                    <div className="w-full">
+                        <p className="text-sm text-gray-300 mb-2">Start Semester:</p>
+                        <select value={startSemester} onChange={(e) => setStartSemester(e.target.value)} className="p-2 border border-gray-300 text-black rounded-lg w-full mb-4">
+                            <option value="">Select Semester</option>
+                            <option value="Spring">Spring</option>
+                            <option value="Summer">Summer</option>
+                            <option value="Fall">Fall</option>
+                        </select>
+                        <p className="text-sm text-gray-300 mb-2">Start Year:</p>
+                        <input type="number" placeholder="Start Year" value={startYear} onChange={(e) => setStartYear(e.target.value)} className="p-2 border border-gray-300 text-black rounded-lg w-full mb-4" />
+                        <p className="text-sm text-gray-300 mb-2">End Semester:</p>
+                        <select value={endSemester} onChange={(e) => setEndSemester(e.target.value)} className="p-2 border border-gray-300 text-black rounded-lg w-full mb-4">
+                            <option value="">Select Semester</option>
+                            <option value="Spring">Spring</option>
+                            <option value="Summer">Summer</option>
+                            <option value="Fall">Fall</option>
+                        </select>
+                        <p className="text-sm text-gray-300 mb-2">End Year:</p>
+                        <input type="number" placeholder="End Year" value={endYear} onChange={(e) => setEndYear(e.target.value)} className="p-2 border border-gray-300 text-black rounded-lg w-full mb-4" />
+                        <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 mx-auto block">Submit</button>                    
+                    </div>
+                    <h2 className="text-2xl font-bold text-center mt-4">Courses</h2>
+                    <br />
+                    <div className="grid grid-cols-3 gap-4">
+                    {sections && sections.map((section, index) => (
+                        <div key={index} className="w-full p-4 border border-gray-300 rounded-lg mb-4">
+                            <p className="text-lg font-bold">{section.courseNumber}</p>
+                            <p className="text-sm">{section.sectionNumber}</p>
+                            <p className="text-sm">{section.semester} {section.year}</p>
+                        </div>
+                    ))}
+                    </div>
+                    {error && <p className="text-red-500">Error: {error}</p>}
+                    <button onClick={() => router.push("/instructors")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 self-start">Back</button>
+                </div>
+            </main>
+        </>
+    )
+}
